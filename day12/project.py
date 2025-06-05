1f1ae99673e74788801623154711240400f1ff14
shruti lenka <shrutilenka21122002@gmail.com>
2025-06-05 21:34:49 +0530

    fixed error in day12

--- day12/project.py
+++ day12/project.py
@@ -2,35 +2,36 @@ import random
 
 print("ready for a guessing game?")
 print("I'm thinking of a number between 1 and 100.")
-computer = random.randint(1, 101)
-diffi = input("choose a difficulty level: easy or hard?")
-if diffi == "easy":
-    attempts = 10
-else:
-    attempts = 5
-
-print(f"you have {attempts} attempts remaining to guess the number")
 
 
 def main():
     while True:
-        global computer, attempts, diffi
+        computer = random.randint(1, 100)
+        diffi = input("choose a difficulty level: easy or hard?")
+        if diffi == "easy":
+            attempts = 10
+        else:
+            attempts = 5
+        print(f"you have {attempts} attempts remaining to guess the number")
         while attempts > 0:
             guess = int(input("make a guess: "))
             attempts -= 1
-            if guess < computer:
-                print("too low")
-            elif guess > computer:
-                print("too high")
+            if attempts != 0:
+                if guess < computer:
+                    print("too low")
+                elif guess > computer:
+                    print("too high")
+                else:
+                    print("you got it! you win!")
+                    break
+                print(
+                    f"you have {attempts} attempts remaining to guess the number")
             else:
-                print("you got it! you win!")
-                break
-            print(
-                f"you have {attempts} attempts remaining to guess the number")
+                print("you ran out of attempts!")
         print(f"you lose! the number was {computer}")
         play_again = input(
             "do you want to play again? (yes/no): ").strip().lower()
-        if play_again != "yes":
+        if play_again == "no":
             print("thanks for playing!")
             break
 
